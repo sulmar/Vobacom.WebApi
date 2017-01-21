e9ffb00b7104de360cfd865524574f2db309ae01
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-21 14:35:34 +0100

    Utworzenie interfejsu

---
+++ Vobacom.HappyWheeks.Interfaces/IStationsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vobacom.HappyWheels.Models;
+
+namespace Vobacom.HappyWheeks.Interfaces
+{
+    public interface IStationsService
+    {
+        IList<Station> Get();
+
+        Station Get(int id);
+
+        void Add(Station station);
+
+        void Update(Station station);
+
+        void Delete(int id);
+    }
+}
