9ae479feffccd982cf9e4dd9438fa3b375ff28c4
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-28 17:49:48 +0100

    Utworzenie klienta MVC

---
+++ Vobacom.HappyWheels.MVCClient/Controllers/BikesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Vobacom.HappyWheeks.Interfaces;
+using Vobacom.HappyWheels.RestApiServices;
+
+namespace Vobacom.HappyWheels.MVCClient.Controllers
+{
+    public class BikesController : Controller
+    {
+        private IBikesService bikesService;
+
+        public BikesController()
+            : this(new BikesRestApiService())
+        { }
+
+        public BikesController(IBikesService bikesService)
+        {
+            this.bikesService = bikesService;
+        }
+
+
+        // GET: Bikes
+        public async Task<ActionResult> Index()
+        {
+            var bikes = await bikesService.GetAsync();
+
+            return View(bikes);
+        }
+    }
+}
---
+++ Vobacom.HappyWheels.MVCClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vobacom.HappyWheels.MVCClient.MvcApplication" Language="C#" %>
