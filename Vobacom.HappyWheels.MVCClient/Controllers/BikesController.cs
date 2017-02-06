fe62b9d5ddf0da7875bade477743f4a438884399
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-06 16:53:24 +0100

    Walidacja w MVC za pomocą Fluent Validation

--- Vobacom.HappyWheels.MVCClient/Controllers/BikesController.cs
+++ Vobacom.HappyWheels.MVCClient/Controllers/BikesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Vobacom.HappyWheeks.Interfaces;
+using Vobacom.HappyWheels.Models;
 using Vobacom.HappyWheels.RestApiServices;
 
 namespace Vobacom.HappyWheels.MVCClient.Controllers
@@ -29,6 +30,19 @@ namespace Vobacom.HappyWheels.MVCClient.Controllers
             var bikes = await bikesService.GetAsync();
 
             return View(bikes);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Bike bike)
+        {
+
+            if (!ModelState.IsValid)
+            { // re-render the view when validation failed.
+                return View("Create", bike);
+            }
+
+            return RedirectToAction("Index");
+
         }
     }
 }
--- Vobacom.HappyWheels.MVCClient/Global.asax.cs
+++ Vobacom.HappyWheels.MVCClient/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace Vobacom.HappyWheels.MVCClient
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FluentValidationModelValidatorProvider.Configure();
         }
     }
 }
